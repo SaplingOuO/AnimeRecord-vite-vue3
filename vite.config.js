cb9c24ddc7febf38f147bbb1d947612fa0de8aab
SBG_DESKTOP <gba2006su06@gmail.com>
2025-03-24 21:54:41 +0800

    no message

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//   ],
+//   base: process.env.NODE_ENV === 'production' ? 'AnimeRecord-vite-vue3' : '/',
+// })
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  base: process.env.NODE_ENV === 'production' ? 'AnimeRecord-vite-vue3' : '/',
-})
+  plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? 'AnimeRecord-vite-vue3' : '/', // ✅ GitHub Pages 的專案路徑
+});
